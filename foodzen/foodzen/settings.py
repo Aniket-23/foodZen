fa815409b12fec33abce04359c43999b7411dd5b
Aniket-23 <117341962+Aniket-23@users.noreply.github.com>
2024-05-22 23:26:58 +0530

    created a base app and connected it to main project

--- foodzen/foodzen/settings.py
+++ foodzen/foodzen/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'base.apps.BaseConfig',
 ]
 
 ROOT_URLCONF = 'foodzen.urls'
